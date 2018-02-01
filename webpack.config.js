7754b8ba1fb28b5fb13f0cdf10629f00fc0343ba
Matthew Carpowich <matthewcarpowich@Matthews-MacBook-Air.local>
2018-02-01 14:13:09 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+const config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+    extensions: ['*', '.js']
+  },
+  // module: {
+  //   loaders: [
+  //     {
+  //       test: /\.jsx?/,
+  //       include: APP_DIR,
+  //       loader: 'babel-loader'
+  //     }
+  //   ]
+  // }
+}
+
+module.exports = config;
